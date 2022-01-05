f87b8f203bec4e05aa2d7feb1e2f67221960231c
Edd <edd_kleszcz@hotmail.co.uk>
2022-01-05 15:47:03 +0000

    Adding base entity to classes, closes #10

--- data/src/main/java/guru/springframework/petclinicproject/model/Pet.java
+++ data/src/main/java/guru/springframework/petclinicproject/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.petclinicproject.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity s{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
