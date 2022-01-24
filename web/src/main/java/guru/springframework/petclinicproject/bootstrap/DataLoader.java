67e82a87bd6f31af88fa644b324f3a2b2f828fa4
Edd <edd_kleszcz@hotmail.co.uk>
2022-01-24 17:19:19 +0000

    Closes #15, added vet list and controller

--- web/src/main/java/guru/springframework/petclinicproject/bootstrap/DataLoader.java
+++ web/src/main/java/guru/springframework/petclinicproject/bootstrap/DataLoader.java
@@ -46,7 +46,7 @@ public class DataLoader implements CommandLineRunner {
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         vetService.save(vet2);
-
         System.out.println("Loaded Vets...");
+
     }
 }
