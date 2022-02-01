b973bce4e7aa0d22ff224fbc46992b04cb3676ee
Edd <edd_kleszcz@hotmail.co.uk>
2022-02-01 18:08:59 +0000

    Creating speciality service, closes #31

---
+++ data/src/main/java/guru/springframework/petclinicproject/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinicproject.services;
+
+import guru.springframework.petclinicproject.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
