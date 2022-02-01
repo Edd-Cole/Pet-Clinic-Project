763c9babbd3fa90469dc302a73e4bd06586e7ef2
Edd <edd_kleszcz@hotmail.co.uk>
2022-02-01 17:35:44 +0000

    Adding in service implementation for petType, closes #29

---
+++ data/src/main/java/guru/springframework/petclinicproject/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinicproject.services;
+
+import guru.springframework.petclinicproject.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
