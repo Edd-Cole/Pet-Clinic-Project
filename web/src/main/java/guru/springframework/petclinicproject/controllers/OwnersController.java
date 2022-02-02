13172cb5aeaed3a8f335fa18b6ce6d321ff6be22
Edd <edd_kleszcz@hotmail.co.uk>
2022-02-02 15:36:16 +0000

    Solving error handling, closes #45

--- web/src/main/java/guru/springframework/petclinicproject/controllers/OwnersController.java
+++ web/src/main/java/guru/springframework/petclinicproject/controllers/OwnersController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("owners")
+@RequestMapping("/owners")
 @Controller
 public class OwnersController {
     private final OwnerService ownerService;
@@ -18,5 +18,10 @@ public class OwnersController {
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners(Model model){
+        return "/notImplemented";
     }
 }
