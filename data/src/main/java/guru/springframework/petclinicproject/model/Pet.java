b414e44c76d65bb06ce170fa83d50e91c4bff1a1
Edd <edd_kleszcz@hotmail.co.uk>
2022-10-01 17:05:44 +0100

    closes #36

--- data/src/main/java/guru/springframework/petclinicproject/model/Pet.java
+++ data/src/main/java/guru/springframework/petclinicproject/model/Pet.java
@@ -1,11 +1,25 @@
 package guru.springframework.petclinicproject.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pet")
 public class Pet extends BaseEntity {
+    
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
