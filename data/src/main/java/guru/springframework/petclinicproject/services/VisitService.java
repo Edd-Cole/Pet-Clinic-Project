efe00f6ea36dbd8d8c3c3ff3a992d80602acbb82
Edd <edd_kleszcz@hotmail.co.uk>
2022-10-02 09:31:47 +0100

    closes #46

---
+++ data/src/main/java/guru/springframework/petclinicproject/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.petclinicproject.services;
+
+import guru.springframework.petclinicproject.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
