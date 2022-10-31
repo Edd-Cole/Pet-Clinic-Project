4adcf5d0d789ae9e86e1ac5085b2de68c28f8295
Edd <edd_kleszcz@hotmail.co.uk>
2022-10-31 20:53:45 +0000

    closes #4

---
+++ web/src/test/java/guru/springframework/petclinicproject/PetClinicProjectApplicationTest.java
@@ -0,0 +1,14 @@
+package guru.springframework.petclinicproject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PetClinicProjectApplicationTest {
+  
+  @Test
+  public void contextLoads() {}
+}
